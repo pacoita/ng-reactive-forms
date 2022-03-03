62bc6581a62ed8586ff87e8ca2ae0a3dd8d2506e
Francesco Leardini <piolag@gmail.com>
2022-03-03 21:47:00 +0100

    Added cross validation component and route (skeleton)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrossValidationComponent } from './cross-validation/cross-validation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'cross', pathMatch: 'full' },
+  { path: 'cross', component: CrossValidationComponent },
+  { path: '**', redirectTo: 'cross' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/cross-validation/cross-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cross-validation',
+  templateUrl: './cross-validation.component.html',
+  styleUrls: ['./cross-validation.component.scss']
+})
+export class CrossValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
