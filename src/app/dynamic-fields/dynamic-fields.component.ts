9fc03dba1d56961f3489b6f6a780d5a2c4a46b30
Francesco Leardini <piolag@gmail.com>
2022-03-04 18:29:40 +0100

    Completed dynamic fields demo

--- src/app/dynamic-fields/dynamic-fields.component.ts
+++ src/app/dynamic-fields/dynamic-fields.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-fields',
   templateUrl: './dynamic-fields.component.html',
-  styleUrls: ['./dynamic-fields.component.scss']
+  styleUrls: ['./dynamic-fields.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DynamicFieldsComponent implements OnInit {
+export class DynamicFieldsComponent {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get firstname() {
+    return this.addressForm.get('firstname');
+  }
+  get lastname() {
+    return this.addressForm.get('lastname');
+  }
+  get addresses() {
+    return this.addressForm.get('addresses') as FormArray;
   }
 
+  showValueCtrl = this.fb.control(false);
+
+  addressForm = this.fb.group({
+    firstname: [''],
+    lastname: [''],
+    addresses: this.fb.array([]),
+  });
+
+  newAddress(): FormGroup {
+    return this.fb.group({
+      street: '',
+      city: '',
+    });
+  }
+  addAddress(): void {
+    const emptyAddress = this.newAddress();
+    this.addresses.push(emptyAddress);
+  }
+
+  removeAddress(i: number) {
+    this.addresses.removeAt(i);
+  }
 }
