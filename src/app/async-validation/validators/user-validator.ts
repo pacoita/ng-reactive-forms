5ab6aca6a8bc4237d87708cf5798b42b1fe0d47f
Francesco Leardini <piolag@gmail.com>
2022-03-04 15:53:20 +0100

    Updated title and link text

--- src/app/async-validation/validators/user-validator.ts
+++ src/app/async-validation/validators/user-validator.ts
@@ -3,17 +3,17 @@ import {
   AsyncValidatorFn,
   ValidationErrors,
 } from '@angular/forms';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { UserService } from 'src/app/shared/services/user.service';
 
 export class UserValidator {
   static usernameValidator(userService: UserService): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
-      return userService
-        .checkIfUsernameExists(control.value)
-        .pipe(
-          map((result: boolean) => (result ? { usernameExists: true } : null))
-        );
+      return userService.checkIfUsernameExists(control.value).pipe(
+        map((result: boolean) => (result ? { usernameExists: true } : null)),
+        // If an error occurs, we could decide to generate a specific ValidationError
+        catchError(() => of(null))
+      );
     };
   }
 }
