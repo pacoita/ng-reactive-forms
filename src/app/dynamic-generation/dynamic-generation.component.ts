b85f022c760fe8fecd163c355a9b25c66b634835
Francesco Leardini <piolag@gmail.com>
2022-03-04 18:40:39 +0100

    Added dynamic generation component (only structure).

---
+++ src/app/dynamic-generation/dynamic-generation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-generation',
+  templateUrl: './dynamic-generation.component.html',
+  styleUrls: ['./dynamic-generation.component.scss']
+})
+export class DynamicGenerationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/dynamic-generation/dynamic-generation.module.ts
+++ src/app/dynamic-generation/dynamic-generation.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DynamicGenerationRoutingModule } from './dynamic-generation-routing.module';
+import { DynamicGenerationComponent } from './dynamic-generation.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DynamicGenerationComponent
+  ],
   imports: [
     CommonModule,
     DynamicGenerationRoutingModule
