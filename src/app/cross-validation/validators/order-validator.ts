7bdf4b02702f28a6a94b0b9df4cc2a3097d72fc4
Francesco Leardini <francesco.leardini@mobi.ch>
2022-08-13 12:27:05 +0200

    set single import

--- src/app/cross-validation/validators/order-validator.ts
+++ src/app/cross-validation/validators/order-validator.ts
@@ -1,9 +1,5 @@
-import { ValidationErrors } from '@angular/forms';
-import { ValidatorFn } from '@angular/forms';
-import { AbstractControl } from '@angular/forms';
-export const orderValidator: ValidatorFn = (
-  control: AbstractControl
-): ValidationErrors | null => {
+import { ValidationErrors, ValidatorFn, AbstractControl } from '@angular/forms';
+export const orderValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const qty = control.get('qty')?.value;
   const qtyConfirm = control.get('qtyConfirm')?.value;
 
